8a2cce3e0fcb92b20fb98c4416509c6fc607f9a5
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-02-11 14:39:00 +0200

    ppp2

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-const g = 334;
+const g = 334 - 666;
 const lkdkfld = "adfadfajkds";
-
-const ff = 444 -1;
-
-const fdd = "adada";
-
-console.log("ffff2");
-console.error("sdkasjdlsjkda");
-
+const ff = 444 + 555;
