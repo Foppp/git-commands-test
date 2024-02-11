2cb8185a5b5ad2e5c675000cf90745e7a147efb4
Yura Chemeris <53577903+Foppp@users.noreply.github.com>
2024-02-11 14:35:37 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const g = 334;
 const lkdkfld = "adfadfajkds";
-const ff = 444;
 
-console.error("sdkasjdlsjkda");
+const ff = 444 -1;
+
 const fdd = "adada";
+
 console.log("ffff2");
+console.error("sdkasjdlsjkda");
 
